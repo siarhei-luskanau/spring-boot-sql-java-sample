76f88dd6ff487d9bef691a7ff3b4aa8123bddc2e
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-04-06 14:58:24 +0200

    Spring Boot Docker Compose Support

--- src/main/java/siarhei/luskanau/sql/example/demo/DemoApplication.java
+++ src/main/java/siarhei/luskanau/sql/example/demo/DemoApplication.java
@@ -2,9 +2,11 @@ package siarhei.luskanau.sql.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJdbcRepositories
 @EnableScheduling
 public class DemoApplication {
 
--- src/test/java/siarhei/luskanau/sql/example/demo/DemoApplicationTests.java
+++ src/test/java/siarhei/luskanau/sql/example/demo/DemoApplicationTests.java
@@ -2,9 +2,19 @@ package siarhei.luskanau.sql.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
+@Testcontainers
 @SpringBootTest
 class DemoApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
 
     @Test
     void contextLoads() {
